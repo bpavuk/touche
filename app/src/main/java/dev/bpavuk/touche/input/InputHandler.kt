0b9cafc429387b5299c7da0d3e717bd96e8b9c06
bpavuk <boulderobscura@gmail.com>
2025-02-22 18:26:53 +0200

    fixed weird sensitivity difference between vertical and horizontal movements

--- app/src/main/java/dev/bpavuk/touche/input/InputHandler.kt
+++ app/src/main/java/dev/bpavuk/touche/input/InputHandler.kt
@@ -2,6 +2,7 @@ package dev.bpavuk.touche.input
 
 import dev.bpavuk.touche.input.types.ToucheInput
 import dev.bpavuk.touche.usb.UsbConnection
+import kotlin.math.min
 
 interface InputHandler {
     suspend fun pass(input: List<ToucheInput>)
@@ -11,12 +12,18 @@ class UsbInputHandler(private val connection: UsbConnection) : InputHandler {
     private fun List<ToucheInput>.compress(): ByteArray = (
             joinToString("\n") {
                 when (it) {
-                    is ToucheInput.Finger -> buildString {
-                        append("F\t")
-                        append("${it.offset.x * 4096 / it.screenSize.x}\t")
-                        append("${it.offset.y * 4096 / it.screenSize.y}\t")
-                        append(if (it.pressed) "1\t" else "0\t")
-                        append(it.id.value)
+                    is ToucheInput.Finger -> {
+                        val scale = min(
+                            4096 / it.screenSize.x.toFloat(),
+                            4096 / it.screenSize.y.toFloat()
+                        )
+                        buildString {
+                            append("F\t")
+                            append("${it.offset.x * scale}\t")
+                            append("${it.offset.y * scale}\t")
+                            append(if (it.pressed) "1\t" else "0\t")
+                            append(it.id.value)
+                        }
                     }
 
                     is ToucheInput.Stylus -> buildString {
