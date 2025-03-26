9d1070feb32daae2fda25af9ae5602e8f49ee265
bpavuk <75901693+bpavuk@users.noreply.github.com>
2025-03-26 14:02:15 +0200

    watcher state discarding flag

--- app/src/main/java/dev/bpavuk/touche/input/WatcherViewModel.kt
+++ app/src/main/java/dev/bpavuk/touche/input/WatcherViewModel.kt
@@ -2,8 +2,8 @@ package dev.bpavuk.touche.input
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import dev.bpavuk.touche.input.types.ToucheScreenSize
 import dev.bpavuk.touche.input.types.ToucheInput
+import dev.bpavuk.touche.input.types.ToucheScreenSize
 import dev.bpavuk.touche.usb.UsbConnection
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
@@ -14,24 +14,39 @@ class WatcherViewModel(private val connection: UsbConnection) : ViewModel() {
 
     fun watch() {
         viewModelScope.launch(Dispatchers.IO) {
-            while (true) {
+            opcodeLoop@while (true) {
                 val opcode = connection.read()
 
-                while (state == WatcherState.initial || state.encode(opcode) == null) {
-                    delay(1000 / 60) // polling state at 60Hz
-                }
+                stateLoop@while (true) {
+                    // TODO: rewrite with non-local break once Kotlin 2.2 is out
+                    val encodedState = state.encode(opcode) ?: continue@stateLoop
 
-                state.encode(opcode)?.let { connection.write(it) }
+                    delay(5) // poll state at 200Hz
+                    connection.write(encodedState)
+                    break@stateLoop
+
+                    // Kotlin 2.2 variant
+//                    state.encode(opcode)?.let { encodedState ->
+//                        connection.write(encodedState)
+//                        break@stateLoop;
+//                    } ?: delay(1000 / 60)
+                }
             }
         }
     }
 
     fun pass(input: List<ToucheInput>) {
-        state = state.copy(input = input)
+        state = WatcherState(
+            input = input,
+            screen = state.screen
+        )
     }
 
     fun setScreenState(screen: ToucheScreenSize) {
-        state = state.copy(screen = screen)
+        state = WatcherState(
+            input = state.input,
+            screen = screen
+        )
     }
 }
 
@@ -42,11 +57,22 @@ class InvalidOpcodeException(opcode: Int) : IllegalStateException(
 private data class WatcherState(
     val input: List<ToucheInput>, val screen: ToucheScreenSize
 ) {
+    override fun toString(): String = "discard: $discard; input: $input, screen: $screen"
+
+    private var discard = false
+
+    private constructor(
+        input: List<ToucheInput>, screen: ToucheScreenSize, discard: Boolean
+    ) : this(input, screen) {
+        this.discard = discard
+    }
+
     companion object {
-        val initial = WatcherState(emptyList(), ToucheScreenSize(0, 0))
+        val initial = WatcherState(emptyList(), ToucheScreenSize(0, 0), true)
     }
 
     fun encode(opcode: Int): ByteArray? {
+        if (discard) return null
         return when (opcode) {
             1 -> {
                 input.encode()
@@ -61,6 +87,7 @@ private data class WatcherState(
     }
 }
 
-private fun List<ToucheInput>.encode(): ByteArray? = map { it.encode() }.reduceOrNull { acc, toucheInput ->
-    acc + "\n".encodeToByteArray() + toucheInput
-}
+private fun List<ToucheInput>.encode(): ByteArray? =
+    map { it.encode() }.reduceOrNull { acc, toucheInput ->
+        acc + "\n".encodeToByteArray() + toucheInput
+    }
