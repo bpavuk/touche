484dc017a5704cbc7ff39ea4d763f59b6caf6c21
bpavuk <boulderobscura@gmail.com>
2026-03-16 19:55:13 +0200

    fix(ui): cramped settings button on narrow devices in landscape
    On quite a number of phones, the Settings button was cramped in
    landscape orientation. Making the home screen scrollable is a clutch
    from UX point of view; true adaptive design is coming.

--- app/src/main/java/dev/bpavuk/touche/ui/screens/home/Home.kt
+++ app/src/main/java/dev/bpavuk/touche/ui/screens/home/Home.kt
@@ -9,6 +9,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -124,6 +126,8 @@ fun HomeScreen(
         Column(
             modifier = Modifier
                 .padding(paddingValues)
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState())
                 .padding(horizontal = 16.dp),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
